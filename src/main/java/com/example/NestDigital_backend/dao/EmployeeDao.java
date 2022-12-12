62d04373b86c1a97aeb8121f8dfce7a24d4e1c71
priyaaranjith <ranjithpriya307@gmail.com>
2022-12-12 16:05:45 +0530

    viewprofile

--- src/main/java/com/example/NestDigital_backend/dao/EmployeeDao.java
+++ src/main/java/com/example/NestDigital_backend/dao/EmployeeDao.java
@@ -2,6 +2,8 @@ package com.example.NestDigital_backend.dao;
 
 import com.example.NestDigital_backend.model.Employees;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -16,5 +18,13 @@ public interface EmployeeDao extends CrudRepository<Employees,Integer> {
     @Query(value = "SELECT `id`, `designation`, `email`, `empid`, `name`, `password`, `phone`, `salary`, `username` FROM `employees` WHERE `empid` LIKE %:empid%",nativeQuery = true)
     List<Employees>SearchEmployee (@Param("empid") Integer empid);
 
+    @Modifying
+    @Transactional
+
+    @Query(value = "DELETE FROM `employees` WHERE `id`= :id",nativeQuery = true)
+    void DeleteEmployee(@Param("id") Integer id);
+
+    @Query(value = "SELECT `id`, `designation`, `email`, `empid`, `name`, `password`, `phone`, `salary`, `username` FROM `employees` WHERE `id`= :id",nativeQuery = true)
+    List<Employees>ViewEmployee(@Param("id") Integer id);
 
 }
